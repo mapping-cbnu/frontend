efd74b676bbc15fdc96c8d604cd16c1aa68dfc80
Minnnning <6alswjd6@gmail.com>
2023-08-25 16:14:57 +0900

    회원가입 페이지 수정 이메일 추가

--- src/components/user/Signup.tsx
+++ src/components/user/Signup.tsx
@@ -1,22 +1,29 @@
 import { SetStateAction, useState } from 'react';
+import axios from 'axios';
 import './css/user.css';
 
 // 회원가입
 function Signup() {
-  const [Id, setId] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [checkingPassword, setCheckingPassword] = useState('');
   const [message, setMessage] = useState('');
   const [blankMessage, setBlankMessage] = useState('');
-  const onChangeId = (e: { target: { value: SetStateAction<string> } }) => {
-    setId(e.target.value);
+
+  const onChangeEmail = (e: { target: { value: SetStateAction<string> } }) => {
+    setEmail(e.target.value);
+  };
+  const onChangeName = (e: { target: { value: SetStateAction<string> } }) => {
+    setName(e.target.value);
   };
   const onChangePassword = (e: {
     target: { value: SetStateAction<string> };
   }) => {
     setPassword(e.target.value);
   };
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onChangeCheckingPassword = (e: { target: { value: any } }) => {
     const currentPassword = e.target.value;
@@ -29,12 +36,34 @@ function Signup() {
     if (currentPassword === '') setMessage('');
   };
   const checkingBlank = () => {
-    if (Id && password && checkingPassword) {
+    if (email && password && checkingPassword && name) {
       setBlankMessage('');
     } else {
       setBlankMessage('빈칸이 있습니다.');
     }
   };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('checkingPassword', checkingPassword);
+    formData.append('nickname', name);
+
+    try {
+      await axios.post('', formData, {
+        headers: { Accept: 'application/json' },
+      });
+      console.log('회원이 성공적으로 생성되었습니다.');
+      window.location.reload(); //메인이 아니라 로그인 페이지로 이동해야 한다
+    } catch (error) {
+      console.error('회원 생성에 실패했습니다.', error);
+    }
+  };
+
   return (
     <div className="container">
       <h1>회원가입</h1>
@@ -42,11 +71,21 @@ function Signup() {
       <form action="" method="post">
         <div className="input__block">
           <input
-            type="text"
-            placeholder="아이디"
+            type="email"
+            placeholder="이메일"
             className="input"
-            id="ID"
-            onChange={onChangeId}
+            id="email"
+            onChange={onChangeEmail}
+          />
+        </div>
+        <div className="input__block">
+          <input
+            type="text"
+            placeholder="닉네임"
+            className="input"
+            id="name"
+            maxLength={8}
+            onChange={onChangeName}
           />
         </div>
         <div className="input__block">
@@ -71,13 +110,15 @@ function Signup() {
         <div className={blankMessage ? 'message-active' : ''}>
           {blankMessage}
         </div>
-        <button
-          type={blankMessage ? 'button' : 'submit'}
-          className="signup__btn"
-          onClick={checkingBlank}
-        >
-          회원가입
-        </button>
+        {blankMessage ? (
+          <button type="button" className="signup__btn" onClick={checkingBlank}>
+            회원가입
+          </button>
+        ) : (
+          <button type="submit" className="signup__btn" onClick={handleSubmit}>
+            회원가입
+          </button>
+        )}
       </form>
     </div>
   );
