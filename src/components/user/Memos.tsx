c4e2433e148bd76cd008fd8f3f8b9fd78c66baf6
Minnnning <6alswjd6@gmail.com>
2023-08-25 23:55:08 +0900

    이미지 css 수정

--- src/components/user/Memos.tsx
+++ src/components/user/Memos.tsx
@@ -1,19 +1,21 @@
 import './css/user.css';
 import './css/button.css';
-import Modify from './Modify';
+//import Modify from './Modify';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
 function Memo(props: any) {
   return (
     <>
-      <Modify data={props.data} />
+      {/* <Modify data={props.data} /> */}
       <li>
         <div className="memo-title-container">
           <div className="memo-title">{props.data.tag}</div>
           <div>{props.data.date}</div>
         </div>
         <div className="memo-content-container">
-          <img className="memo-img" src={props.data.img} />
+          {props.data.img != 'null' && (
+            <img className="memo-img" src={props.data.img} />
+          )}
           <div className="memo-content">{props.data.content}</div>
         </div>
         <div className="memo-button-container">
